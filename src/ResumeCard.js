c555d084288477de8b98656f6243829882971f36
Godfrey Yeung <godfreyyeung@users.noreply.github.com>
2020-02-29 15:46:13 -0500

    Add placeholder for company logos on exp Cards

--- src/ResumeCard.js
+++ src/ResumeCard.js
@@ -17,27 +17,47 @@ const CardHeader = (props) => {
             <Grid
                 container
                 item
-                justify="space-between"
-                alignItems="baseline"
-            >
+            >   
                 <Grid item>
-                    <Typography variant="h6">
-                        {props.info.company} <Typography variant="caption">({props.info.city}, {props.info.state})</Typography>
-                    </Typography>
+                    <div
+                        style={
+                            {
+                                width: '50px',
+                                height: '75px',
+                                'background-color': 'silver',
+                                'margin-right': '10px'
+                            }
+                        }>
+                    </div>
                 </Grid>
-                <Grid item>
-                    <Typography variant="subtitle2"> {props.info.startDate} - {props.info.endDate} </Typography>
+                <Grid
+                    item
+                >
+                    <Grid
+                        container
+                        item
+                        justify="space-between"
+                        alignItems="baseline"
+                    >
+                        <Grid item>
+                            <Typography variant="h6">
+                                {props.info.company} <Typography variant="caption">({props.info.city}, {props.info.state})</Typography>
+                            </Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="subtitle2"> {props.info.startDate} - {props.info.endDate} </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid>
+                        <Typography variant="overline">
+                            {props.info.role}
+                        </Typography>
+                    </Grid>
                 </Grid>
-            </Grid>
-            <Grid>
-                <Typography variant="overline">
-                    {props.info.role}
-                </Typography>
             </Grid>
         </Grid>
     )
 }
-
 
 class ResumeCard extends React.Component {
     state = {
@@ -54,10 +74,6 @@ class ResumeCard extends React.Component {
         if (showBack) {
             return (
                 <div>
-                    <CardHeader
-                        info={this.props.info}
-                        onClick={this.flipOver}
-                    />
                     <div>
                         <ul>
                             {this.props.info.achievements.map(achievement => ( <li>{achievement}</li> )) }
@@ -69,16 +85,6 @@ class ResumeCard extends React.Component {
                     <ul>
                         {this.props.info.projects.map(achievement => ( <li>{achievement}</li> )) }
                     </ul>
-                    
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <CardHeader
-                        info={this.props.info}
-                        onClick={this.flipOver}
-                    />
                 </div>
             )
         }
@@ -94,10 +100,16 @@ class ResumeCard extends React.Component {
                         {
                             padding: '15px',
                             'min-height': '50px',
-                            'margin-bottom': '10px',
+                            'border-radius': '0',
+                            margin: '0 0 5px 0'
                         }
                     }
+                    className="not-hovered-resume-card"
                 >
+                    <CardHeader
+                        info={this.props.info}
+                        onClick={this.flipOver}
+                    />
                     {this.cardContent(this.state.showBack)}
                 </Card>
             </Grid>
